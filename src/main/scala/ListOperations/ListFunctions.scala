f95a0437609d1f9e9ef5ee701733eaa89b3827dc
swati joshi <swati.joshi@worldpay.com>
2018-08-23 20:23:00 +0100

    updated code based on sam's suggestions.

--- src/main/scala/ListOperations/ListFunctions.scala
+++ src/main/scala/ListOperations/ListFunctions.scala
@@ -6,23 +6,20 @@ object ListFunctions {
     l.foreach(println)
   }
 
-  def multiplyByTen(list: List[Int]): List[Int] = {
-    val output: List[Int] = list.map(listElemet => {
-      listElemet * 10
-    })
-    output
-  }
+  def multiplyByTen(list: List[Int]): List[Int] =
+    //list.map(Int => {Int * 10})
+    list.map(_ * 10)
 
   //def isEven(number: List[Int]): Boolean = number % 2 == 0
 
   def multiplyByTenEven(list: List[Int]) : Unit ={
     val evens = list.filter(_% 2 == 0)
      println(evens)
-    val result : List [Int] = evens.map(Int => Int * 10)
+    val result : List [Int] = evens.map(_ * 10)
     println(result)
     val odds = list.filterNot(_% 2 == 0)
     println(odds)
-    val result2 : List [Int] = odds.map(Int => Int * 100)
+    val result2 : List [Int] = odds.map(_ * 100)
     println(result2)
     }
 }
