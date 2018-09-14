36c8a1efd2cacc471304d764f7a655c35fd3a957
swati joshi <swati.joshi@worldpay.com>
2018-09-14 11:19:00 +0100

    few more examples

--- src/main/scala/ListOperations/ListFunctions.scala
+++ src/main/scala/ListOperations/ListFunctions.scala
@@ -12,14 +12,28 @@ object ListFunctions {
 
   //def isEven(number: List[Int]): Boolean = number % 2 == 0
 
-  def multiplyByTenEven(list: List[Int]) : Unit ={
-    val evens = list.filter(_% 2 == 0)
-     println(evens)
-    val result : List [Int] = evens.map(_ * 10)
-    println(result)
-    val odds = list.filterNot(_% 2 == 0)
-    println(odds)
-    val result2 : List [Int] = odds.map(_ * 100)
-    println(result2)
+  def multiplyByTenEven(list: List[Int]) : Unit = {
+    val evens = list.filter(_ % 2 == 0)
+    //println(evens)
+    val result: List[Int] = evens.map(_ * 10)
+    //println(result)
+  }
+    def multiplyByHunderdOdd(list: List[Int]) : Unit = {
+      val odds = list.filterNot(_ % 2 == 0)
+      println(odds)
+      val result2: List[Int] = odds.map(_ * 100)
+      }
+
+    def count (input: List[String]) : Unit = {
+      val i  = input.head
+      val tail =input.tail
+      println(tail)
+      println(i)
     }
+    def sum(input: List[Int]) : Int =
+      input.foldLeft(0)((r,c) => r+c)
+
+
+
+
 }
--- src/main/scala/ListOperations/ListMain.scala
+++ src/main/scala/ListOperations/ListMain.scala
@@ -5,12 +5,19 @@ object ListMain {
 
   def main(args: Array[String]) {
     val list1: List[Int] = List(1, 2, 3, 4)
-    ListFunctions.prettyPrintList(list1)
+    println(ListFunctions.prettyPrintList(list1))
     val list2: List[String] = List("a", "b")
     ListFunctions.prettyPrintList(list2)
     println("Filter even")
-    ListFunctions.multiplyByTenEven(list1)
-    }
+    println(ListFunctions.multiplyByTenEven(list1))
+    println("multiplyByTen")
+    println(ListFunctions.multiplyByTen(list1))
+    println("multiplyByHundred")
+    println(ListFunctions.multiplyByHunderdOdd(list1))
+    val list3: List[String] = List("abc","123","fc", "abc")
+    ListFunctions.count(list3)
+    println(ListFunctions.sum(list1))
+  }
 }
 
 
